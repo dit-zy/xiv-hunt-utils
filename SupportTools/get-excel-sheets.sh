1eb1f98bf50bc2024880898918945cb2e29830b3
ditzy <ditzy.zy@pm.me>
2024-09-03 02:30:38 -0500

    add a github workflow to update hunt data (#1)

---
+++ SupportTools/get-excel-sheets.sh
@@ -0,0 +1,15 @@
+﻿#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+function get_data_csv() {
+  csv="$1"
+  url="https://raw.githubusercontent.com/xivapi/ffxiv-datamining/master/csv/${csv}.csv"
+  echo "getting: $url"
+  curl -o "$url" | tail -n +2 >> "/tmp/$csv"
+}
+
+get_data_csv Map
+get_data_csv PlaceName
+get_data_csv TerritoryType
+get_data_csv TerritoryTypeTransient
---
+++ SupportTools/update_map_metadata.js
@@ -0,0 +1,11 @@
+﻿const fs = require('node:fs')
+const {parse} = require('csv-parse/sync')
+
+function getCsvData(filename) {
+	return parse(fs.readFileSync(`/tmp/${filename}.csv`), {columns: true,})
+}
+
+const maps = getCsvData('Map')
+const placeNames = getCsvData('PlaceName')
+const territoryTypes = getCsvData('TerritoryType')
+const territoryTypeTransients = getCsvData('TerritoryTypeTransient')
