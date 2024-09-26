a5a47232d2291582f118c1ee09603d7820144109
ditzy <ditzy.zy@pm.me>
2024-09-26 00:31:48 -0500

    add aetheryte data to hunt data json

--- SupportTools/update_hunt_data.js
+++ SupportTools/update_hunt_data.js
@@ -1,34 +1,47 @@
 ﻿const fs = require('node:fs')
 require('./data_utils.js')
 
+const MAPS_WITHOUT_AETHERYTES = new Set([
+	'the dravanian hinterlands',
+])
+
 console.log('parsing the raw spawn data...')
 const rawSpawnData = JSON.parse(fs.readFileSync('SupportTools/raw_spawn_data.json'))
+
+console.log('parsing the raw aetheryte data...')
+const rawAetheryteData = JSON.parse(fs.readFileSync('SupportTools/raw_aetheryte_data.json'))
+const aetheryteData = rawAetheryteData
+	.filter(aetheryteData => aetheryteData.CanTeleport)
+	.reduce((acc, aetheryteData) => {
+		acc.getOrDefault(aetheryteData.Zone)[aetheryteData.AetheryteName.toLowerCase()] = aetheryteData.Position
+		return acc
+	}, {})
+// console.log(JSON.stringify(aetheryteData))
+
 console.log('parsing the map metadata...')
 const rawMapMetadata = JSON.parse(fs.readFileSync('SupportTools/map_data.json'))
-const mapMetadata = rawMapMetadata
+const {mapMetadata, mapNames} = rawMapMetadata
 	.ownKeys()
 	.reduce((acc, patch) => {
 		rawMapMetadata[patch]
 			.ownKeys()
 			.forEach(mapName => {
-				acc[mapName] = {
+				acc.mapMetadata[mapName] = {
 					patch: patch,
 					zOffset: rawMapMetadata[patch][mapName].zOffset,
 				}
+				acc.mapNames[rawMapMetadata[patch][mapName].id] = mapName
 			})
 		return acc
-	}, {})
+	}, {mapMetadata: {}, mapNames: {}})
+// console.log(JSON.stringify(mapMetadata))
+// console.log(JSON.stringify(mapNames))
 
-const patches = {}
-const maps = {}
-const marks = {}
-const spawns = []
-
-function rawToMapPos(pos, map, patch) {
+function rawToMapPos(pos, map) {
 	const rawX = pos.X
 	const rawY = pos.Y
 	const rawZ = pos.Z
-	const scale = patch == "hw" ? 95 : 100
+	const scale = mapMetadata[map].patch == "hw" ? 95 : 100
 	return {
 		x: 2048 / scale + rawX / 50 + 1,
 		y: 2048 / scale + rawZ / 50 + 1,
@@ -52,6 +65,12 @@ function indentS(content, numIndents = 1) {
 
 // === parse the raw data ===
 console.log('consuming the raw data...')
+
+const patches = {}
+const maps = {}
+const marks = {}
+const spawns = []
+const aetherytes = {}
 rawSpawnData.forEach((spawn, i) => {
 	const mapName = spawn.ZoneName.toLowerCase()
 	const markNames = spawn.MobNames.map(mobName => mobName.toLowerCase())
@@ -73,8 +92,22 @@ rawSpawnData.forEach((spawn, i) => {
 	)
 
 	// spawn points
-	spawns.push(rawToMapPos(rawSpawnPos, mapName, patch))
+	spawns.push(rawToMapPos(rawSpawnPos, mapName))
 })
+
+// aetherytes
+aetheryteData
+	.ownKeys()
+	.filter(mapId => mapNames.hasOwnProperty(mapId))
+	.forEach(mapId => {
+	const mapName = mapNames[mapId]
+	aetheryteData[mapId]
+		.ownKeys()
+		.forEach(aetheryteName => {
+			aetherytes.getOrDefault(mapName)[aetheryteName] = rawToMapPos(aetheryteData[mapId][aetheryteName], mapName)
+		})
+})
+// console.log(JSON.stringify(aetherytes))
 
 // === generate the json ===
 // this is done manually, instead of using JSON.stringify(), so the formatting
@@ -89,8 +122,37 @@ const patchesJson = patches.ownKeys().map(patch => {
 
 		const markKeyLength = mapMarks.max(mark => mark.length) + 2
 		const mapMarksJson = mapMarks.map(markName =>
-			`${`"${markName}"`.padEnd(markKeyLength)} : { "spawns": [${marks[markName][mapName].join(',')}] }`
+			`${`"${markName}"`.padEnd(markKeyLength)}: { "spawns": [${marks[markName][mapName].join(',')}] }`
 		).join(',\n')
+
+		let mapAetherytesJson = ''
+		if (!aetherytes.hasOwnProperty(mapName)) {
+			if (!MAPS_WITHOUT_AETHERYTES.has(mapName)) {
+				throw new Error(`map [${mapName}] has no aetherytes, but is not in the maps-without-aetherytes allow list`)
+			}
+			console.log(`no aetherytes found for map [${mapName}]`)
+			mapAetherytesJson = '"aetherytes": {},'
+		} else {
+			const aetheryteNameLength = aetherytes[mapName].ownKeys().max(name => name.length) + 2
+			const aetherytesJson = aetherytes[mapName]
+				.ownKeys()
+				.map(aetheryteName => {
+					const aetherytePos = aetherytes[mapName][aetheryteName];
+					return [
+						`${`"${aetheryteName}"`.padEnd(aetheryteNameLength)}: {`,
+						`"x": ${aetherytePos.x.toFixed(2).padStart(5)}, `,
+						`"y": ${aetherytePos.y.toFixed(2).padStart(5)}, `,
+						`"z": ${aetherytePos.z.toFixed(2).padStart(5)}`,
+						`}`,
+					].join('')
+				}).join(',\n')
+			mapAetherytesJson = [
+				'"aetherytes": {',
+				indentS(aetherytesJson),
+				'},'
+				].join('\n')
+		}
+
 		const mapSpawnsJson = mapSpawns.map(spawnId => {
 			const spawn = spawns[spawnId];
 			return [
@@ -108,6 +170,7 @@ const patchesJson = patches.ownKeys().map(patch => {
 				'"marks": {',
 				indentS(mapMarksJson),
 				'},',
+				mapAetherytesJson,
 				'"spawns": [',
 				indentS(mapSpawnsJson),
 				']',
