951c81084792d6195f8594d4e4d0620b775731a7
ditzy <ditzy.zy@pm.me>
2025-03-28 14:28:25 -0500

    remove default instance territories

--- XIVHuntUtils/HuntConstants.cs
+++ XIVHuntUtils/HuntConstants.cs
@@ -5,11 +5,6 @@ namespace XIVHuntUtils;
 public static class HuntConstants {
 	public static readonly (string Version, IList<(Territory, uint)> Instances) LatestPatchIncreasedInstances =
 		("7.2", [
-			(Territory.Urqopacha, 1),
-			(Territory.Kozamauka, 1),
-			(Territory.YakTel, 1),
-			(Territory.Shaaloani, 1),
 			(Territory.HeritageFound, 2),
-			(Territory.LivingMemory, 1)
 		]);
 }
