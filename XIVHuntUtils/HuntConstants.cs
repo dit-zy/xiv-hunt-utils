0189790a6474f7f7a3e698ce6a5b773b394c80b0
ditzy <ditzy.zy@pm.me>
2025-04-22 23:14:38 -0500

    update instances for patch 7.25

--- XIVHuntUtils/HuntConstants.cs
+++ XIVHuntUtils/HuntConstants.cs
@@ -4,7 +4,7 @@ namespace XIVHuntUtils;
 
 public static class HuntConstants {
 	public static readonly (string Version, IList<(Territory, uint)> Instances) LatestPatchIncreasedInstances =
-		("7.2", [
-			(Territory.HeritageFound, 2),
+		("7.25", [
+			(Territory.MareLamentorum, 3),
 		]);
 }
