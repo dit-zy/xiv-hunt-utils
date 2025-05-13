55ac75c559f2c99400fec21e64999b8f388fc96c
ditzy <ditzy.zy@pm.me>
2025-05-12 21:19:23 -0500

    add patch extension for territories

--- XIVHuntUtils/Models/Patch.cs
+++ XIVHuntUtils/Models/Patch.cs
@@ -1,6 +1,7 @@
 ﻿// ReSharper disable InconsistentNaming
 
 using DitzyExtensions.Collection;
+using DitzyExtensions.Functional;
 using static XIVHuntUtils.Models.Territory;
 
 namespace XIVHuntUtils.Models;
@@ -16,43 +17,48 @@ public enum Patch {
 
 public static class PatchExtensions {
 	private static readonly IDictionary<Patch, IList<Territory>> PatchHuntMaps = new (Patch, IList<Territory>)[] {
-			(Patch.ARR, new[] {
+			(Patch.ARR, [
 				LimsaLominsaLowerDecks, UldahStepsOfNald, NewGridania,
 				MiddleLaNoscea, LowerLaNoscea, EasternLaNoscea, WesternLaNoscea, UpperLaNoscea, OuterLaNoscea,
 				WesternThanalan, CentralThanalan, EasternThanalan, SouthernThanalan, NorthernThanalan,
 				CentralShroud, EastShroud, SouthShroud, NorthShroud,
 				CoerthasCentralHighlands, MorDhona
-			}),
-
-			(Patch.HW, new[] {
+			]),
+			(Patch.HW, [
 				Foundation, Idyllshire,
 				CoerthasWesternHighlands, TheSeaOfClouds, AzysLla,
-				TheDravanianForelands, TheDravanianHinterlands, TheChurningMists,
-			}),
-			(Patch.SB, new[] {
+				TheDravanianForelands, TheDravanianHinterlands, TheChurningMists
+			]),
+			(Patch.SB, [
 				Kugane, RhalgrsReach, TheDomanEnclave,
 				TheFringes, ThePeaks, TheLochs,
-				TheRubySea, Yanxia, TheAzimSteppe,
-			}),
-			(Patch.SHB, new[] {
+				TheRubySea, Yanxia, TheAzimSteppe
+			]),
+			(Patch.SHB, [
 				TheCrystarium, Eulmore,
 				Lakeland, Kholusia, AmhAraeng,
-				IlMheg, TheRaktikaGreatwood, TheTempest,
-			}),
-			(Patch.EW, new[] {
+				IlMheg, TheRaktikaGreatwood, TheTempest
+			]),
+			(Patch.EW, [
 				OldSharlayan, RadzAtHan,
 				Labyrinthos, Thavnair, Garlemald,
-				MareLamentorum, Elpis, UltimaThule,
-			}),
-			(Patch.DT, new[] {
+				MareLamentorum, Elpis, UltimaThule
+			]),
+			(Patch.DT, [
 				Tuliyollal, SolutionNine,
 				Urqopacha, Kozamauka, YakTel,
-				Shaaloani, HeritageFound, LivingMemory,
-			}),
+				Shaaloani, HeritageFound, LivingMemory
+			]),
 		}
 		.Select(patch => (patch.Item1, patch.Item2.AsList()))
 		.AsDict()
 		.VerifyEnumDictionary();
+
+	private static readonly IDictionary<Territory, Patch> TerritoryPatches =
+		PatchHuntMaps
+			.SelectMany(patch => patch.Value.Zip(patch.Key.Repeat(patch.Value.Count)))
+			.AsDict()
+			.VerifyEnumDictionary();
 
 	private static readonly IDictionary<Patch, string> PatchEmotes = new Dictionary<Patch, string> {
 		{ Patch.ARR, ":2x:" },
@@ -64,6 +70,8 @@ public static class PatchExtensions {
 	}.VerifyEnumDictionary();
 
 	public static IList<Territory> HuntMaps(this Patch patch) => PatchHuntMaps[patch];
+	
+	public static Patch ContainingPatch(this Territory territory) => TerritoryPatches[territory];
 
 	public static uint MaxMarks(this Patch patch) {
 		if (patch == Patch.ARR) return 17;
