782d6f1bd5f15be5b8d16479f868fbd63a30797d
ditzy <ditzy.zy@pm.me>
2025-08-10 21:30:56 -0500

    update for dalamud api13

--- XIVHuntUtils/HuntConstants.cs
+++ XIVHuntUtils/HuntConstants.cs
@@ -1,10 +1,13 @@
-﻿using XIVHuntUtils.Models;
+﻿using DitzyExtensions.Collection;
+using XIVHuntUtils.Models;
 
 namespace XIVHuntUtils;
 
 public static class HuntConstants {
 	public static readonly (string Version, IList<(Territory, uint)> Instances) LatestPatchIncreasedInstances =
-		("7.251", [
-			(Territory.YakTel, 2),
-		]);
+		("7.3", new List<(Territory, uint)> {
+			(Territory.Shaaloani, 2),
+			(Territory.HeritageFound, 3),
+			(Territory.LivingMemory, 2),
+		}.AsList());
 }
