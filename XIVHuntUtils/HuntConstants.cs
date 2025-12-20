930e66b6081378a4bbfc07c94cb2c73ecda2fb87
ditzy <ditzy.zy@pm.me>
2025-12-20 16:11:10 -0600

    update instance counts

--- XIVHuntUtils/HuntConstants.cs
+++ XIVHuntUtils/HuntConstants.cs
@@ -5,9 +5,7 @@ namespace XIVHuntUtils;
 
 public static class HuntConstants {
 	public static readonly (string Version, IList<(Territory, uint)> Instances) LatestPatchIncreasedInstances =
-		("7.3", new List<(Territory, uint)> {
-			(Territory.Shaaloani, 2),
-			(Territory.HeritageFound, 3),
+		("7.4", new List<(Territory, uint)> {
 			(Territory.LivingMemory, 2),
 		}.AsList());
 }
